641429a827d5b72a8dcfe11e91389e27d763fda5
aperoli0401 <aperoli0401@g.educaand.es>
2026-03-24 09:50:20 +0100

    Ejercicio2

---
+++ AlePer_Ej2-Pru_UD4/src/main/java/com/mycompany/aleper_ej2/pru_ud4/AlePer_Ej2Pru_UD4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aleper_ej2.pru_ud4;
+
+/**
+ *
+ * @author 03_1DAW_Alum
+ */
+public class AlePer_Ej2Pru_UD4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ AlePer_Ej2-Pru_UD4/src/test/java/com/mycompany/aleper_ej2/pru_ud4/conversor.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aleper_ej2.pru_ud4;
+
+/**
+ *
+ * @author 03_1DAW_Alum
+ */
+class conversor {
+
+    static void main(String[] argumentos) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static float celsiusAFahrenheit(float celsius) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static float fahrenheitACelsius(float fahrenheit) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
